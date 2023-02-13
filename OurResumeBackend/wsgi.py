23a22572b4b241efebabc9e17847e1354c3fb079
$GH_USERNAME <$GH_USERNAME@users.noreply.github.com>
2023-02-13 05:16:53 +0500

    auto commit by py script

--- OurResumeBackend/wsgi.py
+++ OurResumeBackend/wsgi.py
@@ -6,6 +6,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoBoilerplate.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                      'OurResumeBackend.settings.dev')
 
 application = get_wsgi_application()

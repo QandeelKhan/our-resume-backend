43d4715d23eb0a821a5cb66035ef7656da227d34
$GH_USERNAME <$GH_USERNAME@users.noreply.github.com>
2023-02-14 18:14:34 +0500

    auto commit by py script

--- OurResumeBackend/cdn/storage_conf.py
+++ OurResumeBackend/cdn/storage_conf.py
@@ -14,24 +14,28 @@ if USE_SPACES:
     AWS_S3_ENDPOINT_URL = config('AWS_S3_ENDPOINT_URL')
     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
     # static settings
-    # STATIC_URL = f'https://our-space.nyc3.digitaloceanspaces.com/{AWS_LOCATION}'
     # AWS_LOCATION = 'space-our-resume/static'
+    # STATIC_URL = f'https://our-space.nyc3.digitaloceanspaces.com/{AWS_LOCATION}'
     # public media settings
-    # PUBLIC_MEDIA_LOCATION = 'space-our-resume/media'
+    PUBLIC_MEDIA_LOCATION = 'space-our-resume/media'
     # MEDIA_URL = f'https://our-space.nyc3.digitaloceanspaces.com/{PUBLIC_MEDIA_LOCATION}'
 
     AWS_LOCATION = f'https://our-space.nyc3.digitaloceanspaces.com'
     DEFAULT_FILE_STORAGE = 'OurResumeBackend.cdn.backends.MediaRootS3Boto3Storage'
     STATICFILES_STORAGE = 'OurResumeBackend.cdn.backends.StaticRootS3Boto3Storage'
+    STATIC_URL = '/static/'
+    STATIC_ROOT = BASE_DIR / "space-our-resume/static"
+    MEDIA_URL = '/media/'
+    MEDIA_ROOT = BASE_DIR / 'space-our-resume/media'
 else:
     STATIC_URL = '/static/'
     STATIC_ROOT = BASE_DIR / "space-our-resume/static"
     MEDIA_URL = '/media/'
     MEDIA_ROOT = BASE_DIR / 'space-our-resume/media'
 
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'space-our-resume/'),
-# )
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'space-our-resume/'),
+]
 # helping material
 # https://testdriven.io/blog/django-digitalocean-spaces/
 # https://our-space.nyc3.digitaloceanspaces.com

0c695502b5e81546452f1939b4f106accc878c29
$GH_USERNAME <$GH_USERNAME@users.noreply.github.com>
2023-02-18 15:30:10 +0500

    auto commit by py script

--- OurResumeBackend/cdn/storage_conf.py
+++ OurResumeBackend/cdn/storage_conf.py
@@ -39,6 +39,8 @@
 import os
 from decouple import config
 from OurResumeBackend.settings.common import BASE_DIR
+from dotenv import load_dotenv
+load_dotenv()
 
 USE_SPACES = config('USE_SPACES', cast=bool, default=False)
 
@@ -47,11 +49,11 @@ STATIC_ROOT = BASE_DIR / "space-our-resume/static"
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'space-our-resume/media'
 
-AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
 AWS_DEFAULT_ACL = 'public-read'
-AWS_S3_ENDPOINT_URL = config('AWS_S3_ENDPOINT_URL')
+AWS_S3_ENDPOINT_URL = os.getenv('AWS_S3_ENDPOINT_URL')
 AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
 DEFAULT_FILE_STORAGE = 'OurResumeBackend.cdn.backends.S3Boto3Storage'
 # static settings

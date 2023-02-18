60df50de6039fd4ad7372f902b4c931c8d31bda4
$GH_USERNAME <$GH_USERNAME@users.noreply.github.com>
2023-02-19 00:29:54 +0500

    auto commit by py script

--- OurResumeBackend/cdn/backends.py
+++ OurResumeBackend/cdn/backends.py
@@ -5,7 +5,7 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 
 class StaticRootS3Boto3Storage(S3Boto3Storage):
-    bucket_name = 'our-space'
+    # bucket_name = 'our-space'
     location = "space-our-resume/static"
     default_acl = 'public-read'
 
@@ -14,7 +14,7 @@ class StaticRootS3Boto3Storage(S3Boto3Storage):
 
 
 class MediaRootS3Boto3Storage(S3Boto3Storage):
-    bucket_name = 'our-space'
+    # bucket_name = 'our-space'
     location = "space-our-resume/media"
     default_acl = 'public-read'
 

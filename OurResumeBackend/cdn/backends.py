b4f8b1bd0b935922a52ac868642e640680afa220
$GH_USERNAME <$GH_USERNAME@users.noreply.github.com>
2023-02-19 18:29:20 +0500

    auto commit by py script

--- OurResumeBackend/cdn/backends.py
+++ OurResumeBackend/cdn/backends.py
@@ -5,7 +5,7 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 
 class StaticRootS3Boto3Storage(S3Boto3Storage):
-    # bucket_name = 'our-space'
+    bucket_name = 'our-space'
     location = "static"
     default_acl = 'public-read'
 
--- OurResumeBackend/cdn/storage_conf.py
+++ OurResumeBackend/cdn/storage_conf.py
@@ -2,6 +2,7 @@ import os
 from decouple import config
 from OurResumeBackend.settings.common import BASE_DIR
 
+# USE_SPACES = False
 USE_SPACES = config('USE_SPACES', cast=bool, default=True)
 
 if USE_SPACES:
@@ -11,12 +12,13 @@ if USE_SPACES:
     AWS_DEFAULT_ACL = 'public-read'
     AWS_S3_ENDPOINT_URL = config('AWS_S3_ENDPOINT_URL')
     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
-    AWS_LOCATION = 'our-space/space-our-resume/static'
+    AWS_LOCATION = 'space-our-resume/static'
     PUBLIC_MEDIA_LOCATION = 'space-our-resume/media'
     STATIC_URL = f'{AWS_S3_ENDPOINT_URL}/{AWS_LOCATION}/'
     MEDIA_URL = f'{AWS_S3_ENDPOINT_URL}/{PUBLIC_MEDIA_LOCATION}/'
     DEFAULT_FILE_STORAGE = 'OurResumeBackend.cdn.backends.MediaRootS3Boto3Storage'
-    STATICFILES_STORAGE = 'OurResumeBackend.cdn.backends.StaticRootS3Boto3Storage'
+    # STATICFILES_STORAGE = 'OurResumeBackend.cdn.backends.StaticRootS3Boto3Storage'
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
 else:
     DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
 

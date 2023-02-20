ef6b1ec9a1229fc898350645b0be674f070c1760
$GH_USERNAME <$GH_USERNAME@users.noreply.github.com>
2023-02-21 00:45:37 +0500

    auto commit by py script

--- OurResumeBackend/cdn/backends.py
+++ OurResumeBackend/cdn/backends.py
@@ -1,12 +1,10 @@
 from storages.backends.s3boto3 import S3Boto3Storage
-
 # making settings for our static assets to after that set in conf.py
 # this will make upload our static folder into digitalocean,
 
 
 class StaticRootS3Boto3Storage(S3Boto3Storage):
-    bucket_name = 'our-space'
-    location = "space-our-resume/static"
+    location = "static"
     default_acl = 'public-read'
 
 
@@ -14,9 +12,9 @@ class StaticRootS3Boto3Storage(S3Boto3Storage):
 
 
 class MediaRootS3Boto3Storage(S3Boto3Storage):
-    bucket_name = 'our-space'
-    location = "space-our-resume/media"
+    location = "media"
     default_acl = 'public-read'
+
 
 # above provided locations we set similar to our local settings so the directories
 # name and locations on the our hosted app on cloud look similar to our local file
--- OurResumeBackend/cdn/storage_conf.py
+++ OurResumeBackend/cdn/storage_conf.py
@@ -1,62 +1,35 @@
 import os
 from decouple import config
 from OurResumeBackend.settings.common import BASE_DIR
-from dotenv import load_dotenv
-load_dotenv()
-
 
 USE_SPACES = config('USE_SPACES', cast=bool, default=True)
 
-AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
-AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
-print(AWS_STORAGE_BUCKET_NAME)
-AWS_S3_ENDPOINT_URL = os.getenv("AWS_S3_ENDPOINT_URL")
-AWS_DEFAULT_ACL = "public-read"
-AWS_S3_OBJECT_PARAMETERS = {
-    'CacheControl': 'max-age=86400',
-}
-
-
 if USE_SPACES:
-    # DigitalOcean Spaces settings
-    # DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-    # # DEFAULT_FILE_STORAGE = 'OurResumeBackend.cdn.backends.MediaRootS3Boto3Storage'
-    # # STATICFILES_STORAGE = 'OurResumeBackend.cdn.backends.StaticRootS3Boto3Storage'
+    # settings
+    AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+    AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+    AWS_DEFAULT_ACL = 'public-read'
+    AWS_S3_ENDPOINT_URL = config('AWS_S3_ENDPOINT_URL')
+    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+    # static settings
+    AWS_LOCATION = 'space-shoppingly/static'
+    STATIC_URL = f'https://our-space.nyc3.digitaloceanspaces.com/{AWS_LOCATION}/'
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    # public media settings
+    PUBLIC_MEDIA_LOCATION = 'space-our-resume/media'
+    MEDIA_URL = f'https://our-space.nyc3.digitaloceanspaces.com/{PUBLIC_MEDIA_LOCATION}/'
 
-    # Use DigitalOcean Spaces for static and media files
-    # STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-    STATICFILES_STORAGE = 'OurResumeBackend.cdn.backends.StaticRootS3Boto3Storage'
-    # DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
     DEFAULT_FILE_STORAGE = 'OurResumeBackend.cdn.backends.MediaRootS3Boto3Storage'
-
-    # Custom domain for serving static files from Spaces
-    # AWS_S3_CUSTOM_DOMAIN = f"https://{AWS_STORAGE_BUCKET_NAME}.{AWS_S3_ENDPOINT_URL}"
-    # print(AWS_S3_CUSTOM_DOMAIN)
-
-    # Static file settings
-    # STATIC_URL = f"{AWS_S3_ENDPOINT_URL}/our-space/space-our-resume/"
-    STATIC_URL = 'https://our-space.nyc3.digitaloceanspaces.com/our-space/space-our-resume/'
-    print(STATIC_URL)
-    STATIC_ROOT = None
-
-    # Media file settings
-    MEDIA_URL = f"{AWS_S3_ENDPOINT_URL}/space-our-resume/media/"
-    MEDIA_ROOT = None
-    STATICFILES_STORAGE = DEFAULT_FILE_STORAGE
-    # Django storage settings
-    AWS_LOCATION = 'space-our-resume'  # set to a subdirectory if desired
-    STATICFILES_LOCATION = 'space-our-resume/static'
-    MEDIAFILES_LOCATION = 'space-our-resume/media'
 else:
-    # Django default storage settings
-    # DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
-    MEDIA_ROOT = os.path.join(BASE_DIR, 'space-our-resume/media')
-    STATIC_ROOT = os.path.join(BASE_DIR, 'space-our-resume/static')
-    MEDIA_URL = '/media/'
     STATIC_URL = '/static/'
+    STATICFILES_DIRS = (
+        BASE_DIR / 'space-shoppingly/static',
+    )
+    STATIC_ROOT = BASE_DIR / "staticfiles"
+    MEDIA_URL = '/media/'
+    MEDIA_ROOT = BASE_DIR / 'space-shoppingly/media'
 
-# cache settings
-AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL = 'public-read'
-AWS_QUERYSTRING_EXPIRE = 3600
+# helping material
+# https://testdriven.io/blog/django-digitalocean-spaces/
+# https://shopingly-space.fra1.digitaloceanspaces.com/media/productimg/0_98drx4MegZUq4iTd.jpeg

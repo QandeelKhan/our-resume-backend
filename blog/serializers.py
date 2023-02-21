2e0f4e9bfb6f8e887a07ea94ec7ec45c09e89424
$GH_USERNAME <$GH_USERNAME@users.noreply.github.com>
2023-02-21 18:19:40 +0500

    auto commit by py script

--- blog/serializers.py
+++ blog/serializers.py
@@ -1,6 +1,6 @@
 
 from rest_framework import serializers
-from .models import BlogPost, BlogPostImage
+from .models import BlogPost, BlogPostImage, Comment
 
 
 class BlogPostImageSerializer(serializers.ModelSerializer):
@@ -16,8 +16,19 @@ class BlogPostImageSerializer(serializers.ModelSerializer):
         return None
 
 
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = '__all__'
+
+
+# class BlogPostSerializer(serializers.ModelSerializer):
+#     comments = CommentSerializer(many=True, read_only=True)
+
+
 class BlogPostSerializer(serializers.ModelSerializer):
     post_images = BlogPostImageSerializer(many=True)
+    comments = CommentSerializer(many=True, read_only=True)
 
     class Meta:
         model = BlogPost

3fc0545d0200b41486be1ccc1cd93c29a1b19113
$GH_USERNAME <$GH_USERNAME@users.noreply.github.com>
2023-02-21 17:00:41 +0500

    auto commit by py script

--- blog/serializers.py
+++ blog/serializers.py
@@ -1,9 +1,24 @@
 
 from rest_framework import serializers
-from .models import BlogPost
+from .models import BlogPost, BlogPostImage
+
+
+class BlogPostImageSerializer(serializers.ModelSerializer):
+    images = serializers.SerializerMethodField()
+
+    class Meta:
+        model = BlogPostImage
+        fields = '__all__'
+
+    def get_images(self, obj):
+        if obj.images:
+            return self.context['request'].build_absolute_uri(obj.images.url)
+        return None
 
 
 class BlogPostSerializer(serializers.ModelSerializer):
+    post_images = BlogPostImageSerializer(many=True)
+
     class Meta:
         model = BlogPost
         fields = '__all__'

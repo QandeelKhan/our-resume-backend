c37a60953a71f2f513b414cf32d8ec1a393d770e
$GH_USERNAME <$GH_USERNAME@users.noreply.github.com>
2023-02-23 07:04:13 +0500

    auto commit by py script

--- UserManagement/admin.py
+++ UserManagement/admin.py
@@ -1,30 +1,67 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from .models import User
 
 
-@admin.register(User)
-class CustomUserAdmin(UserAdmin):
+class UserAdmin(BaseUserAdmin):
     list_display = ('email', 'first_name', 'last_name',
-                    'tc', 'is_staff', 'is_superuser')
-    search_fields = ('email', 'first_name', 'last_name')
+                    'tc', 'is_staff', 'is_active', 'is_superuser',)
+
+    search_fields = ('email', 'first_name', 'last_name',)
     ordering = ('email',)
 
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
         ('Personal Info', {'fields': ('first_name',
          'last_name', 'tc', 'profile_image')}),
-        ('Permissions', {'fields': ('is_active',
+        ('Permissions', {'fields': ('is_active', 'is_staff',
          'is_superuser', 'groups', 'user_permissions')}),
         ('Important dates', {
-         'fields': ('last_login', 'created_at',)}),
+         'fields': ('last_login', 'created_at')}),
     )
 
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'tc', 'first_name', 'last_name', 'password1', 'password2'),
-        }),
+            'fields': ('email', 'tc', 'first_name', 'last_name', 'password1', 'password2', 'is_staff', 'is_active')}
+         ),
     )
 
-    list_filter = ('is_active', 'is_superuser', 'is_staff', 'groups')
+    list_filter = ('is_active', 'is_staff', 'is_superuser',  'groups',)
+
+    def mark_as_active(self, request, queryset):
+        queryset.update(is_active=True)
+    mark_as_active.short_description = "Mark selected users as active"
+
+    def mark_as_inactive(self, request, queryset):
+        queryset.update(is_active=False)
+    mark_as_inactive.short_description = "Mark selected users as inactive"
+
+    def send_email(self, request, queryset):
+        subject = "Important message from our system"
+        message = "Dear user,\n\nWe would like to inform you that..."
+        for user in queryset:
+            user.email_user(subject, message)
+    send_email.short_description = "Send email to selected users"
+
+    def grant_staff_permission(self, request, queryset):
+        queryset.update(is_staff=True)
+        self.message_user(
+            request, "Staff permission granted to selected users.")
+        # return HttpResponseRedirect(reverse('admin:app_list', args=['UserManagement']))
+    grant_staff_permission.short_description = "Grant staff permission to selected users"
+
+    def remove_staff_permission(self, request, queryset):
+        queryset.update(is_staff=False)
+        self.message_user(
+            request, "Staff permission removed from selected users.")
+        # return HttpResponseRedirect(reverse('admin:app_list', args=['UserManagement']))
+    remove_staff_permission.short_description = "Remove staff permission from selected users"
+
+    actions = [mark_as_active, mark_as_inactive,
+               send_email, grant_staff_permission, remove_staff_permission]
+
+
+admin.site.register(User, UserAdmin)
--- UserManagement/models.py
+++ UserManagement/models.py
@@ -8,7 +8,7 @@ from django.core.files.storage import FileSystemStorage
 from PIL import Image
 from django.core.files.storage import default_storage
 from decouple import config
-
+from django.utils.translation import gettext_lazy as _
 
 USE_SPACES = config('USE_SPACES', cast=bool, default=True)
 if USE_SPACES:
@@ -27,20 +27,51 @@ def validate_image(image):
 
 
 class UserManager(BaseUserManager):
+    """
+    Custom user model manager where email is the unique identifiers
+    for authentication instead of usernames.
+    """
+
     def create_user(self, email, tc, first_name, last_name, password=None, **extra_fields):
+        """
+        Create and save a user with the given email and password.
+        """
         if not email:
-            raise ValueError('The Email field must be set')
+            raise ValueError(_("The Email must be set"))
         email = self.normalize_email(email)
         user = self.model(email=email, tc=tc, first_name=first_name,
                           last_name=last_name, **extra_fields)
         user.set_password(password)
-        user.save(using=self._db)
+        user.save()
         return user
 
     def create_superuser(self, email, tc, first_name, last_name, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-        return self.create_user(email, tc, first_name, last_name, password, **extra_fields)
+        """
+        Create and save a SuperUser with the given email and password.
+        """
+        # extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        # extra_fields.setdefault("is_active", True)
+
+        # if extra_fields.get("is_staff") is not True:
+        #     raise ValueError(_("Superuser must have is_staff=True."))
+        # if extra_fields.get("is_superuser") is not True:
+        #     raise ValueError(_("Superuser must have is_superuser=True."))
+        # return self.create_user(email, tc, first_name, last_name, password=None, **extra_fields)
+
+        user = self.create_user(
+            email,
+            tc,
+            first_name,
+            last_name,
+            password,
+            **extra_fields
+        )
+        user.is_admin = True
+        user.is_staff = True
+        user.is_active = True
+        user.save(using=self._db)
+        return user
 
 
 class User(AbstractBaseUser, PermissionsMixin):
@@ -56,7 +87,6 @@ class User(AbstractBaseUser, PermissionsMixin):
                                       storage=fs, validators=[validate_image], blank=True, null=True)
     tc = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
-    is_admin = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
     created_at = models.DateTimeField(default=timezone.now)
     updated_at = models.DateTimeField(auto_now=True)
@@ -83,7 +113,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     # def has_perm(self, perm, obj=None):
     #     "Does the user have a specific permission?"
     #     # Simplest possible answer: Yes, always
-    #     return self.is_admin
+    #     return True
 
     # def has_module_perms(self, app_label):
     #     "Does the user have permissions to view the app `app_label`?"
